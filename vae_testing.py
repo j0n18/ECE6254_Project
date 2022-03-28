58ded7382fa086db5d1d9522c0fc785c8d939a1e
AbdoSharaf98 <asharfal@u.rochester.edu>
2022-03-28 12:27:06 -0400

    added testing for CIFAR and FashionMNIST

--- vae_testing.py
+++ vae_testing.py
@@ -7,19 +7,47 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
+
+def visualize_data(dataset='mnist'):
+    fashion_map = {
+        0: "T-Shirt",
+        1: "Trouser",
+        2: "Pullover",
+        3: "Dress",
+        4: "Coat",
+        5: "Sandal",
+        6: "Shirt",
+        7: "Sneaker",
+        8: "Bag",
+        9: "Ankle Boot"}
+    vis_fig = plt.figure(figsize=(8, 8))
+    cols, rows = 3, 3
+    for i in range(1, cols * rows + 1):
+        idx = np.argwhere(np.array(training_data.targets) == i - 1)
+        img, label = training_data[idx.flatten()[0]]
+        vis_fig.add_subplot(rows, cols, i)
+        if dataset == 'FashionMNIST':
+            plt.title(fashion_map[label])
+        elif dataset == 'CIFAR':
+            plt.title(training_data.classes[label])
+        plt.axis("off")
+        plt.imshow(img.permute(1, 2, 0))
+    plt.show()
+
+
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # download the datasets
 data_dir = r"C:\Users\abdos\Downloads\GT Academics\Spring 2022\ECE 6254 Statistical ML\Project\Datasets"
 
-training_data = datasets.MNIST(
+training_data = datasets.CIFAR100(
     root=data_dir,
     train=True,
     download=True,
     transform=ToTensor()
 )
 
-test_data = datasets.MNIST(
+test_data = datasets.CIFAR100(
     root=data_dir,
     train=False,
     download=True,
@@ -27,25 +55,15 @@ test_data = datasets.MNIST(
 )
 
 """ VISUALIZE DATA (COMMENT OUT IF NOT NEEDED) """
-"""
-vis_fig = plt.figure(figsize=(8, 8))
-cols, rows = 3, 3
-for i in range(1, cols * rows + 1):
-    idx = np.argwhere(training_data.targets == i-1)
-    img, label = training_data[idx.flatten()[0]]
-    vis_fig.add_subplot(rows, cols, i)
-    plt.axis("off")
-    plt.imshow(img.squeeze(), cmap='gray')
-plt.show()
-"""
+visualize_data('CIFAR')
 
 """ CREATE THE DATA LOADERS """
 train_loader = DataLoader(training_data, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=32, shuffle=True)
 
 """ BUILD THE AUTOENCODER """
-input_shape = (1, 28, 28)
-hidden_layers = [32, 64, 128, 256, 512]
+input_shape = training_data[0][0].shape
+hidden_layers = [32, 64, 128, 256, 512, 1024]
 latent_dim = 2
 vae = ConvolutionalVAE(input_shape=input_shape,
                        hidden_layers=hidden_layers,
@@ -56,7 +74,7 @@ vae.set_optimizer(adam_optim)
 vae.to(DEVICE)
 
 """ TRAIN THE MODEL """
-epochs = 50
+epochs = 20
 train_losses = []
 for epoch in range(1, epochs + 1):
     epoch_loss = []
